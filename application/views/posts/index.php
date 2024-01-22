08b17817febc5932d81287e215c2ebc462a0251c
Jaycee Capulong <capulongako16@gmail.com>
2024-01-23 02:46:24 +0800

    third commit

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -4,11 +4,20 @@
         <h2><?= $title ?></h2>
         </div>
         <div class="col-2">
-            <a href="posts/create" class="btn btn-success center position-absolute bottom-0 end-0">Create Post</a>
+            <!-- <a href="posts/create" class="btn btn-success center position-absolute bottom-0 end-0">Create Post</a> -->
+            
+            <button type="button" class="btn btn-success center position-absolute bottom-0 end-0" id="create-post">Create Post</button>
+
         </div>
     </div>
     <hr>
- 
+    
+    <div class="container border" id="create-form">
+        
+        
+    </div>
+
+    <br>
 
 
     <div id="show-post"></div>
@@ -60,5 +69,70 @@
             });
         }
         setInterval(fetchData, 3000); // Update every 3 seconds
+
+
+        // function showForm(){
+        //     var form = "";
+
+        //     form += `
+        //         <form action="">
+        //             <div class="form-group">
+        //                 <label>Title</label>
+        //                 <input type="text" class="form-control" name="title" placeholder="Add Title">
+        //             </div>
+        //             <div class="form-group">
+        //                 <label>Body</label>
+        //                 <textarea type="text" class="form-control" name="body" placeholder="Add Body"></textarea>
+        //             </div><br>
+        //             <div class="row">
+        //                 <div class="col-10"></div>
+        //                 <div class="col-2">            
+        //                     <button type="button" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#exampleModal">Submit</button><br>
+        //                 </div>
+        //             </div>
+        //         </form>
+        //     `;
+        //     $('#create-form').html(form);
+        // }
+
+        var flag = true;
+        $(function () {
+            $("#create-post").click(function (e) {
+                var form = "";
+                var btn = document.getElementById("create-post");
+                if(flag){
+                    form += `
+                    <form action="">
+                        <div class="form-group">
+                            <label>Title</label>
+                            <input type="text" class="form-control" name="title" placeholder="Add Title">
+                        </div>
+                        <div class="form-group">
+                            <label>Body</label>
+                            <textarea type="text" class="form-control" name="body" placeholder="Add Body"></textarea>
+                        </div><br>
+                        <div class="row">
+                            <div class="col-10"></div>
+                            <div class="col-2">            
+                                <button type="button" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#exampleModal">Submit</button><br>
+                            </div>
+                        </div>
+                    </form>
+                `;
+                    btn.style.backgroundColor = "red";
+                    btn.innerHTML = "Cancel";
+
+                }else{
+                    btn.style.backgroundColor = "#5cb85c";
+                    btn.innerHTML = "Create Post";
+                }
+                flag = !flag;
+                $('#create-form').html(form);
+                
+
+                e.preventDefault();
+                alert('User clicked');
+        });
+});
     });
 </script>
